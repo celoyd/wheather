977d57fe8cf2577308635b8f6227b05597b265f8
meetar <github@zoomy.net>
2013-02-03 17:58:40 -0500

    added some error handing

--- buff-cube.py
+++ buff-cube.py
@@ -16,7 +16,7 @@ from numpy import uint32, empty
 #import time
 
 paths = argv[1:-1]
-count = len(paths)
+count = len(paths) # number of images to process
 clean = count/4 + 2 # how many images to output
 
 '''
@@ -45,22 +45,24 @@ outdir = argv[-1]
 if not os.path.isdir(outdir):
 	print 'no such dir %s' % (outdir)
 	exit(1)
-
+else:
+	print 'output dir:' + outdir
 size = None
 a = None # who the hell named these variables?
 
 margin_ptr = 0
 
 for c in range(count):
-	print 'opening %s' % (paths[c])
-	
 	try:
 		img = Image.open(paths[c])
 		i = img.load()
-	except:
+	except IOError as e:
+		print "Error:", e
 		print 'skipping %s' % (paths[c])
 		continue
-	
+
+	print 'loading %s' % (paths[c])
+
 	if size == None:
 		size = img.size
 		a = empty([size[1], size[0], clean + margin_len], dtype=uint32)
@@ -76,13 +78,17 @@ for c in range(count):
 		a.sort()
 		margin_ptr = 0
 
-a.sort() # possible unnecessary, but it's not like we're optimizing heavily
+if not (a == None):
+	a.sort() # possibly unnecessary, but it's not like we're optimizing heavily
 
-for c in range(clean-1):
-	jmg = Image.new('RGB', img.size)
-	j = jmg.load()
-	for x in range(img.size[0]):
-		for y in range(img.size[1]):
-			j[x, y] = unpack(a[y, x, c])
-	print 'saving %s' % (c)
-	jmg.save('%s/%s.png' % (outdir, c))
+	for c in range(clean-1):
+		jmg = Image.new('RGB', img.size)
+		j = jmg.load()
+		for x in range(img.size[0]):
+			for y in range(img.size[1]):
+				j[x, y] = unpack(a[y, x, c])
+		#print 'saving %s' % (c)
+		jmg.save('%s/%s.png' % (outdir, c))
+	print "Finished sorting."
+else:
+	print "No images to process"
