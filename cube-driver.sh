edfe0bb8448237871fc96faf158408f5e52a4ae2
meetar <github@zoomy.net>
2013-02-03 17:57:27 -0500

    removed backgrounding for now

--- cube-driver.sh
+++ cube-driver.sh
@@ -1,7 +1,11 @@
 #!/bin/zsh
-
+# if only one arg, use it twice
+if [ $# -eq 1 ]
+then
+	2=$1
+fi
 mkdir -p cube/{$1..$2}
 
 for slice in {$1..$2}; do
-	python buff-cube.py slice/$slice/*.jpg cube/$slice/ &
+	python buff-cube.py slice/$slice/*.jpg cube/$slice/
 done
