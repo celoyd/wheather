2934ff00934a7a501baed35cdb159c351d0b9f13
root <root@modis.mapbox.com>
2013-02-19 18:25:03 -0500

    more slicer bugs

--- slicey.sh
+++ slicey.sh
@@ -7,6 +7,6 @@ cd raws;
 for J in *.jpg; do
 	for slice in {0..7}; do
 		echo "slicing $J";
-		cat $J | jpegtran -perfect -crop 1024x128+0+$(($slice * 128)) > ../slice/$step/$J;
+		cat $J | jpegtran -perfect -crop 1024x128+0+$(($slice * 128)) > slice/$slice/$J;
 	done;
 done;
